fb6049108b002a66951968af17450f527dbccb61
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-12 23:15:46 +0530

    Create rectangle.c
    Finding the area of a rectangle

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+int  l,b,area;
+
+printf("Enter the length and breadth of the rectangle\n");
+scanf("%d%d",&l,&b);
+area=l*b;
+printf("Area of the rectangle is %d",area);
+
+}
