60c189229c34bb0d3dd98b950ffa788a536eb927
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-22 15:26:52 +0530

    Create 17)vowel2.c
    to check whether an entered character is vowel or consonant using if-else statement.

---
+++ 17)vowel2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+ char ch;
+ if(ch==’a’|| ch==’e’|| ch==’i’|| ch==’o’|| ch==’u’)
+  printf(" the entered character %c is vowel",ch);
+ else
+  printf(" The entered character %c is consonant",ch); 
+ getch()
+}
+
