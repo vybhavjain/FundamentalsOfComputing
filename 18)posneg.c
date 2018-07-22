61f8aedb055c81dae18386655db707ee443e6b3a
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-22 15:28:02 +0530

    Create 18)posneg.c
    program to check whether a given number is positive, negative or zero using if – else if statement.

---
+++ 18)posneg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+ int num;
+ if(num==0)
+  printf(" the number is zero");
+ else  if(num>0)
+  printf("the number is positive");
+ else
+  printf(" the number is negative");
+}
