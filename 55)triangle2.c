1832d56de5d07d71750ad9b4e80e93435b1eebd8
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-08-04 23:19:40 +0530

    Create 55)triangle2.c
    to find whether a given triangle is isosceles scalene or equilateral using functions without arguments, without return type.

---
+++ 55)triangle2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+void triangle();
+
+main( )
+{
+	
+	triangle();
+	
+}
+
+void triangle()
+{
+	int a,b,c ;
+	printf("Enter the sides of the triangle\n");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a==b&&b==c)
+		printf("The triangle is equilateral\n");
+	else if((a==b)||(b==c)||(c==a))
+		printf("The triangle is isosceles\n");
+	else
+		printf("The triangle is scalene\n");
+	
+}
